b0a2ea5a6a331cc26a9ce7c0f1dfb79957d0d3df
eclecticexistential <allofitbra@gmail.com>
2018-05-14 16:22:16 -0700

    Followed along to create an OOPGame, however, made with .NET and since no html page is created content may not show up in the browser. Further testing is required.

---
+++ Program/Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOPGame
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Warrior maximus = new Warrior("Maximus", 1000, 120, 40);
+            Warrior bob = new Warrior("Bob", 1000, 120, 40);
+            Battle.StartFight(maximus, bob);
+
+            Console.ReadLine();
+        }
+    }
+}
